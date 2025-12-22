7cb712a7b87160c4ff612a430c29a5b5b3ef2afb
Neal <nilco142199@gmail.com>
2025-12-22 17:17:01 +0200

    Initial commit

---
+++ src/Index2DTest.java
@@ -0,0 +1,24 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class Index2DTest {
+
+    @org.junit.jupiter.api.Test
+    void getX() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getY() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void distance2D() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testToString() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testEquals() {
+    }
+}
